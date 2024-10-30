718dd9e1de6fc39682efea938e566f4ff987cdfd
SYAAINN <alswo08319@gmail.com>
2024-10-30 14:20:10 +0900

    [refactor/#4] hiltViewModel-navigation 적용

--- app/src/main/java/org/sopt/and/presentation/ui/WavveActivity.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/WavveActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import org.sopt.and.presentation.ui.auth.screen.AuthViewModel
 import org.sopt.and.presentation.ui.navigation.WavveNavHost
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 
@@ -23,17 +22,13 @@ class WavveActivity : ComponentActivity() {
         setContent {
             ANDANDROIDTheme {
                 val navController = rememberNavController()
-                val authViewModel: AuthViewModel by viewModels()
-                Scaffold(
-                    containerColor = MaterialTheme.colorScheme.background,
-                    content = { paddingValues ->
-                        WavveNavHost(
-                            navController = navController,
-                            authViewModel = authViewModel,
-                            modifier = Modifier.padding(paddingValues)
-                        )
-                    }
-                )
+                Scaffold { paddingValues ->
+                    WavveNavHost(
+                        navController = navController,
+                        modifier = Modifier.padding(paddingValues)
+                    )
+                }
+
             }
         }
     }
--- app/src/main/java/org/sopt/and/presentation/ui/navigation/WavveNavHost.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/navigation/WavveNavHost.kt
@@ -6,29 +6,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import org.sopt.and.presentation.ui.auth.navigation.authNavGraph
 import org.sopt.and.presentation.ui.auth.screen.AuthViewModel
 import org.sopt.and.presentation.ui.main.navigation.mainNavGraph
+import org.sopt.and.presentation.ui.main.screen.MainViewModel
 
 @Composable
 fun WavveNavHost(
     navController: NavHostController,
-    authViewModel: AuthViewModel,
     modifier: Modifier = Modifier,
 ) {
+    val authViewModel: AuthViewModel = hiltViewModel()
+    val mainViewModel: MainViewModel = hiltViewModel()
     Box(
-        modifier = modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
+        modifier = modifier.fillMaxSize()
     ) {
         NavHost(
             navController = navController,
             startDestination = WavveRoute.AUTH,
         ) {
             authNavGraph(navController, authViewModel)
-            mainNavGraph(navController)
+            mainNavGraph(navController, mainViewModel)
         }
     }
 }
