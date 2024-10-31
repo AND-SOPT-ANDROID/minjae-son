58744a09f36779cd35f64af851b36205b89a2f5d
SYAAINN <alswo08319@gmail.com>
2024-10-31 15:16:49 +0900

    [move/#6] WavveActivity 위치 이동

--- app/src/main/java/org/sopt/and/presentation/ui/navigation/WavveActivity.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/navigation/WavveActivity.kt
@@ -1,17 +1,14 @@
-package org.sopt.and.presentation.ui
+package org.sopt.and.presentation.ui.navigation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import org.sopt.and.presentation.ui.navigation.WavveNavHost
 import org.sopt.and.ui.theme.ANDANDROIDTheme
 
 @AndroidEntryPoint
