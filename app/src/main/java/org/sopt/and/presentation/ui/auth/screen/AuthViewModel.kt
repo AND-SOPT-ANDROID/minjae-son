2996048a1ed2585e24fedb7b15bcbddde9ce66ca
SYAAINN <alswo08319@gmail.com>
2024-11-01 18:13:49 +0900

    [refactor/#6] 로그인, 회원가입 검증부분 확장함수 적용

--- app/src/main/java/org/sopt/and/presentation/ui/auth/screen/AuthViewModel.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/auth/screen/AuthViewModel.kt
@@ -6,72 +6,34 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import org.sopt.and.presentation.model.User
+import org.sopt.and.presentation.model.validateSignUp
 import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor() : ViewModel() {
-    private val _authEmail = MutableLiveData("")
-    val authEmail: LiveData<String> = _authEmail
-
-    private val _authPassword = MutableLiveData("")
-    val authPassword: LiveData<String> = _authPassword
+    private val _user = MutableLiveData<User>()
+    val user: LiveData<User> = _user
 
     private val _signInState = MutableStateFlow<SignInState>(SignInState.Idle)
     val signInState : StateFlow<SignInState> = _signInState
 
-    fun setAuthInfo(
-        authEmail: String,
-        authPassword: String,
-    ) {
-        _authEmail.value = authEmail
-        _authPassword.value = authPassword
-    }
+    private val _signUpState = MutableStateFlow<SignUpState>(SignUpState.Idle)
+    val signUpState : StateFlow<SignUpState> = _signUpState
 
-    fun isSignInValid(
-        inputEmail: String,
-        inputPassword: String
-    ): SignInState {
-        return when {
+    fun validateSignIn(inputEmail: String, inputPassword: String) {
+        _signInState.value = when {
             inputEmail.isEmpty() -> SignInState.EmailEmpty
             inputPassword.isEmpty() -> SignInState.PasswordEmpty
-            _authEmail.value != inputEmail -> SignInState.EmailInvalid
-            _authPassword.value != inputPassword -> SignInState.PasswordInvalid
+            _user.value?.email != inputEmail -> SignInState.EmailInvalid
+            _user.value?.password != inputPassword -> SignInState.PasswordInvalid
             else -> SignInState.Success
         }
     }
 
-    fun updateSignInState(newState: SignInState) {
-        _signInState.value = newState
-    }
-
-    private fun isSignUpEmailValid(email: String): Boolean {
-        return email.matches(EMAIL_VALIDATION_REGEX)
-    }
-
-    private fun isSignUpPasswordValid(password: String): Boolean {
-        val count = listOf(
-            UPPER_CASE_REGEX.containsMatchIn(password),
-            LOWER_CASE_REGEX.containsMatchIn(password),
-            DIGIT_REGEX.containsMatchIn(password),
-            SPECIAL_CHAR_REGEX.containsMatchIn(password)
-        ).count { it }
-
-        return password.length in PW_MIN_LENGTH..PW_MAX_LENGTH && count >= PW_MIN_TYPE_COUNT
-    }
-
-    fun isSignUpValid(email: String, password: String): Boolean {
-        return isSignUpEmailValid(email) && isSignUpPasswordValid(password)
-    }
-
-    companion object {
-        private const val PW_MIN_LENGTH = 8
-        private const val PW_MAX_LENGTH = 20
-        private const val PW_MIN_TYPE_COUNT = 3
-
-        private val EMAIL_VALIDATION_REGEX = "^[a-zA-Z0   -9]+@[a-zA-Z0-9.]+\\.[a-zA-Z]{2,}\$".toRegex()
-        private val UPPER_CASE_REGEX = "[A-Z]".toRegex()
-        private val LOWER_CASE_REGEX = "[a-z]".toRegex()
-        private val DIGIT_REGEX = "[0-9]".toRegex()
-        private val SPECIAL_CHAR_REGEX = "[!@#\$%^&*(),.?\":{}|<>]".toRegex()
+    fun validateSignUp(email: String, password: String) {
+        val authInfo = User(email, password)
+        _signUpState.value = authInfo.validateSignUp()
+        if (_signUpState.value is SignUpState.Success) _user.value = authInfo
     }
 }
