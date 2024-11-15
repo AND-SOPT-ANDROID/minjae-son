376f4584a19b9b952b0b0d457ebd64111051adef
SYAAINN <alswo08319@gmail.com>
2024-11-15 19:12:32 +0900

    [feat/#8] 서버통신 response에 대한 템플릿을 생성합니다.

---
+++ app/src/main/java/org/sopt/and/data/remote/ApiResponse.kt
@@ -0,0 +1,14 @@
+package org.sopt.and.data.remote
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ApiResponse<T>(
+    @SerialName("status")
+    val status: Int,
+    @SerialName("message")
+    val message: String,
+    @SerialName("result")
+    val result: T
+)
