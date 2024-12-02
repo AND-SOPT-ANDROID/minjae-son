22edaeb773cd3ba5e50296bdf4c1b84798a6edc4
SYAAINN <alswo08319@gmail.com>
2024-12-02 20:02:24 +0900

    [feat/#10] 의존성 주입을 위한 module을 생성합니다.

---
+++ app/src/main/java/org/sopt/and/di/DataSourceModule.kt
@@ -0,0 +1,37 @@
+package org.sopt.and.di
+
+import android.content.SharedPreferences
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.and.data.local.datasource.TokenLocalDataSource
+import org.sopt.and.data.local.datasourceimpl.TokenLocalDataSourceImpl
+import org.sopt.and.data.remote.datasource.AuthRemoteDataSource
+import org.sopt.and.data.remote.datasource.UserRemoteDataSource
+import org.sopt.and.data.remote.datasourceimpl.AuthRemoteDataSourceImpl
+import org.sopt.and.data.remote.datasourceimpl.UserRemoteDataSourceImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+    @Binds
+    @Singleton
+    fun bindTokenLocalDataSource(sharedPreferences: SharedPreferences): TokenLocalDataSource {
+        return TokenLocalDataSourceImpl(sharedPreferences)
+    }
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRemoteDataSource(
+        authRemoteDataSourceImpl: AuthRemoteDataSourceImpl
+    ): AuthRemoteDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindUserRemoteDataSource(
+        userRemoteDataSourceImpl: UserRemoteDataSourceImpl
+    ): UserRemoteDataSource
+}
---
+++ app/src/main/java/org/sopt/and/di/NetworkModule.kt
@@ -0,0 +1,56 @@
+package org.sopt.and.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.sopt.and.BuildConfig
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun providesJson(): Json =
+        Json {
+            isLenient = true
+            prettyPrint = true
+            explicitNulls = false
+            ignoreUnknownKeys = true
+        }
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            addInterceptor(loggingInterceptor)
+        }.build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        json: Json
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+}
