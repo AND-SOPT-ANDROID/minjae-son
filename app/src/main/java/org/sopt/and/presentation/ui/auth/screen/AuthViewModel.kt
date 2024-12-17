89f3a3c06122313abd5bf19ca9c93817b3443247
SYAAINN <alswo08319@gmail.com>
2024-12-17 23:56:40 +0900

    [refactor/#12] AuthViewModel 을 수정합니다.

--- app/src/main/java/org/sopt/and/presentation/ui/auth/screen/AuthViewModel.kt
+++ app/src/main/java/org/sopt/and/presentation/ui/auth/screen/AuthViewModel.kt
@@ -1,73 +1,76 @@
 package org.sopt.and.presentation.ui.auth.screen
 
-import android.util.Log
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import org.sopt.and.domain.model.User
 import org.sopt.and.domain.repository.AuthRepository
 import org.sopt.and.domain.repository.TokenRepository
+import org.sopt.and.presentation.util.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(
     private val tokenRepository: TokenRepository,
     private val authRepository: AuthRepository,
-) : ViewModel() {
-    private val _signInState = MutableStateFlow<SignInState>(SignInState.Idle)
-    val signInState: StateFlow<SignInState> = _signInState
+) : BaseViewModel<AuthContract.AuthUiState, AuthContract.AuthSideEffect, AuthContract.AuthEvent>() {
+    override fun createInitialState(): AuthContract.AuthUiState = AuthContract.AuthUiState()
 
-    private val _token = MutableStateFlow("")
-    val token: StateFlow<String> = _token
+    override suspend fun handleEvent(event: AuthContract.AuthEvent) {
+        when (event) {
+            is AuthContract.AuthEvent.OnSignInClicked -> setState {
+                copy(
+                    signInState = event.signInState,
+                )
+            }
+            is AuthContract.AuthEvent.OnSignUpClicked -> setState {
+                copy(
+                    signUpState = event.signUpState,
+                )
+            }
+        }
+    }
 
-    private val _signUpState = MutableStateFlow<SignUpState>(SignUpState.Idle)
-    val signUpState: StateFlow<SignUpState> = _signUpState
-
-    fun validateSignIn(username: String, password: String) {
+    fun validateSignIn() {
         viewModelScope.launch {
-            _signInState.value = SignInState.Loading
-            val result = authRepository.login(username = username, password = password)
-            _signInState.value = result.fold(
+            setEvent(AuthContract.AuthEvent.OnSignInClicked(signInState = SignInState.Loading))
+            authRepository.login(username = currentState.signInUsername, password = currentState.signInPassword).fold(
                 onSuccess = { token ->
                     tokenRepository.setToken(token.token)
-                    SignInState.Success(token)
+                    setEvent(AuthContract.AuthEvent.OnSignInClicked(signInState = SignInState.Success(result = token)))
                 },
                 onFailure = {
-                    SignInState.Failure(it.localizedMessage ?: "에러 발생")
+                    setEvent(AuthContract.AuthEvent.OnSignInClicked(signInState = SignInState.Failure(errorMessage = it.localizedMessage?: "")))
                 }
             )
         }
     }
 
-    fun validateSignUp(username: String, password: String, hobby: String) {
+    fun validateSignUp() {
         viewModelScope.launch {
-            _signUpState.value = SignUpState.Loading
-            val result = authRepository.registerUser(
+            setEvent(AuthContract.AuthEvent.OnSignUpClicked(signUpState = SignUpState.Loading))
+            authRepository.registerUser(
                 user = User(
-                    username = username,
-                    password = password,
-                    hobby = hobby
+                    username = currentState.signUpUsername,
+                    password = currentState.signUpPassword,
+                    hobby = currentState.signUpHobby
                 )
-            )
-            _signUpState.value = result.fold(
-                onSuccess = {
-                    SignUpState.Success(it)
+            ).fold(
+                onSuccess = { userNo ->
+                    setEvent(AuthContract.AuthEvent.OnSignUpClicked(signUpState = SignUpState.Success(userNo)))
                 },
                 onFailure = {
-                    SignUpState.Failure(it.localizedMessage ?: "에러 발생")
+                    setEvent(AuthContract.AuthEvent.OnSignUpClicked(signUpState = SignUpState.Failure(it.localizedMessage ?: "")))
                 }
             )
         }
     }
 
-    fun resetSignInState() {
-        _signInState.value = SignInState.Idle
-    }
-
-    fun resetSignUpState() {
-        _signUpState.value = SignUpState.Idle
-    }
+//    fun resetSignInState() {
+//        _signInState.value = SignInState.Idle
+//    }
+//
+//    fun resetSignUpState() {
+//        _signUpState.value = SignUpState.Idle
+//    }
 }
